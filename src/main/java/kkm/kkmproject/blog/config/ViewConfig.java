c736256bef7fedbd10bcc74869b59995d9f17f48
kkm <kkmaddress@gmail.com>
2024-12-18 20:10:54 +0900

    refactor: Remove license view

--- src/main/java/kkm/kkmproject/blog/config/ViewConfig.java
+++ src/main/java/kkm/kkmproject/blog/config/ViewConfig.java
@@ -9,6 +9,5 @@ public class ViewConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/license").setViewName("license");
     }
 }
