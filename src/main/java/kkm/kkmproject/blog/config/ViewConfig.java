7bbc10d36c12c663266e7bd324c2951b72f121a9
kkm <kkmaddress@gmail.com>
2024-12-22 23:41:00 +0900

    refactor: Apply temporary settings before activating spring security

--- src/main/java/kkm/kkmproject/blog/config/ViewConfig.java
+++ src/main/java/kkm/kkmproject/blog/config/ViewConfig.java
@@ -9,5 +9,6 @@ public class ViewConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/F2106D5E8DEAA4BBEA3B067A9E7A7694648434876819B23AF9372D48F8FF8F42").setViewName("/authed/article");
     }
 }
