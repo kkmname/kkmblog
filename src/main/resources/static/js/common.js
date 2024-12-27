5aa09fede126d2fa568fc52d72e51a39e3e586f0
kkm <kkmaddress@gmail.com>
2024-12-27 09:50:55 +0900

    fix: Temporarily disable localstorage

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -11,7 +11,7 @@ function fetchSubjectListFromAPI() {
   fetch("/api/open/subject")
     .then((response) => response.json())
     .then((data) => {
-      localStorage.setItem("subjectResponseList", JSON.stringify(data));
+      // localStorage.setItem("subjectResponseList", JSON.stringify(data));
       renderSubject(data);
     })
     .catch((error) => {
