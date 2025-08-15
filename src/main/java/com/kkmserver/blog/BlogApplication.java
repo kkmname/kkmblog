7a32a5d5f3e00134888483b67c72ad8d8aeddd68
kkmname <kkmaddress@gmail.com>
2025-08-15 12:14:51 +0900

    feat: Spring-security Temporary Disable

--- src/main/java/com/kkmserver/blog/BlogApplication.java
+++ src/main/java/com/kkmserver/blog/BlogApplication.java
@@ -1,9 +1,16 @@
 package com.kkmserver.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class
+    }
+)
 public class BlogApplication {
 
 	public static void main(String[] args) {
