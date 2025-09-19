c4f560250c18746e3283a0ac1a8d44f6c859a93c
kkmname <kkmaddress@gmail.com>
2025-09-19 14:00:58 +0900

    chore: In the development environment, set up Security settings for H2-Console access.

--- src/main/java/com/kkmserver/blog/config/SecurityConfig.java
+++ src/main/java/com/kkmserver/blog/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions().sameOrigin())
             .authorizeHttpRequests(
                 auth -> auth
                             .requestMatchers(
